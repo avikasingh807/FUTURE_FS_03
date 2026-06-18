fb2daafdf433abd23a1f6dcad1be6f8316322a86
avikasingh807 <avikadangi2006@gmail.com>
2026-06-18 22:43:34 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-const texts = ["Elegant Style", "Modern Fashion", "Luxury Collection"];
-let i = 0;
-
-setInterval(() => {
-  document.getElementById("changing-text").innerText = texts[i];
-  i = (i + 1) % texts.length;
-}, 2000);
-</script>
